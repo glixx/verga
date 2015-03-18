3cbc5e24ffff9026d9faa0571431a963dc6f641e
Skvortsov <Skvortsov@localhost>
2015-03-18 18:06:27 +0300

    Small changes

--- src/verga/net.h
+++ src/verga/net.h
@@ -1,5 +1,7 @@
 /****************************************************************************
+    This file is part of Verga++ Verilog simulator
     Copyright (C) 1987-2015 by Jeffery P. Hansen
+    Copyright (C) 2015 by Andrey V. Skvortsov
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
